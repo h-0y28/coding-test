44a754ff0cda622a7fd92feb7aeeba7db56f28f4
효주 <164720957+h-0y28@users.noreply.github.com>
2024-09-04 20:36:43 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    numbers.sort((a,b)=>b-a);
+    const num = numbers[0] * numbers[1];
+    const num2 = numbers[numbers.length-1] * numbers[numbers.length-2]
+    return num>num2?num:num2;
+}
