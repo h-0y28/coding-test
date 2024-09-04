f33aad910e220705abd4d5792f85cec9b8335b22
hyojoo <jhj080802@naver.com>
2024-09-04 20:15:34 +0900

    240904-문자열은 length를 제공해준다.

---
+++ Lv0/편지.js
@@ -0,0 +1,13 @@
+// my
+
+function solution(message) {
+  return message.split("").length * 2;
+}
+
+//another
+
+function solution(message) {
+  return message.length * 2;
+}
+
+// 문자열은 length를 지원해준다 => 배열로 만들 필요가 없음 ⭐⭐⭐⭐
