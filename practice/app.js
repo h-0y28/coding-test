7af498b2d50078988a379fda63ac1a9d972e9e4a
hyojoo <jhj080802@naver.com>
2024-09-04 20:29:25 +0900

    app

--- practice/app.js
+++ practice/app.js
@@ -1,8 +1,19 @@
-function solution(n) {
-  var answer = 0;
+function solution(arr) {
+  let max1 = Math.max(arr[0], arr[1]);
+  let max2 = Math.min(arr[0], arr[1]);
 
-  for (let i = 1; i < n; i * 10) answer += n % i;
-  console.log(answer);
+  for (let i = 2; i < arr.length; i++) {
+    const num = arr[i];
+    if (num > max1) {
+      max2 = max1;
+      max1 = num;
+    } else if (num > max2) {
+      max2 = num;
+    }
+  }
+
+  return max1 * max2;
 }
 
-solution(123);
+// 예시 실행
+console.log(solution([1, 2, 3, 4, 5]));
