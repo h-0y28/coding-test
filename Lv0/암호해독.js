446dc8648d4103c19433212e2872345c3ba58d6f
hyojoo <jhj080802@naver.com>
2024-09-05 21:05:29 +0900

    240905

---
+++ Lv0/암호해독.js
@@ -0,0 +1,11 @@
+// my
+
+function solution(cipher, code) {
+  var answer = [];
+  for (var i = code - 1; i < cipher.length; i += code) {
+    answer.push(cipher[i]);
+  }
+  return answer.join("");
+}
+
+// another
