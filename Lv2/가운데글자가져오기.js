254ef48d95c95d700e16fab97aa6267297a2b5e3
hyojoo <jhj080802@naver.com>
2024-09-13 23:58:25 +0900

    240913 - fix error

--- Lv2/가운데글자가져오기.js
+++ Lv2/가운데글자가져오기.js
@@ -9,4 +9,13 @@ function solution(s) {
   }
 }
 
-//
+// fixed my code
+
+function solution(s) {
+  var answer = s.split("");
+  if (s.length % 2 === 0) {
+    return answer[s.length / 2 - 1] + answer[s.length / 2];
+  } else {
+    return answer[Math.floor(s.length / 2)];
+  }
+}
