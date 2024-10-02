40cab0e06ba0c19c3cd051f0dc8b7589625cdf16
hyojoo <jhj080802@naver.com>
2024-10-02 23:56:49 +0900

    241002

--- Lv0/369게임.js
+++ Lv0/369게임.js
@@ -6,3 +6,10 @@ function solution(order) {
     .split("")
     .filter((digit) => ["3", "6", "9"].includes(digit)).length; // 3, 6, 9가 포함된 자리만 필터링
 }
+
+// another
+
+function solution(order) {
+  var answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
+  return answer;
+}
