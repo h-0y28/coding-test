7e452ff956d8dd3bb7771bb1d5c3e1a73da28466
hyojoo <jhj080802@naver.com>
2024-10-02 23:59:11 +0900

    241002

--- practice/app.js
+++ practice/app.js
@@ -1,10 +1,13 @@
-function solution(order) {
-  return order
-    .toString()
-    .split("")
-    .filter((a) => Number(a) % 3 === 0).length;
+function solution(n) {
+  var answer = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      answer.push(i);
+    }
+  }
+  return answer;
 }
 
 // 예시 실행
-console.log(solution(200)); // 0
-console.log(solution(29423363)); // 2
+console.log(solution(24)); // 0
+console.log(solution(29)); // 2
